aec55f9b9baf0074e93dcb42d87952c1df90dd5a
zzinue <zzinue@gmail.com>
2021-11-29 13:39:30 -0600

    noticias

--- proy_final/nintendo-site/src/Components/NoticiasEventos/CardNoticias.jsx
+++ proy_final/nintendo-site/src/Components/NoticiasEventos/CardNoticias.jsx
@@ -1,6 +1,8 @@
-import React from 'react'
+import React, {useState, useEffect  } from 'react'
 import CardImageNot from "./CardImageNot";
 import CardBodyNot from "./CardBodyNot";
+import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
+import Loader from "react-loader-spinner";
 
 
 const CardNoticias = ({img}) => {
@@ -12,6 +14,8 @@ const CardNoticias = ({img}) => {
         width: "300px",
     }
     const [items, setItems] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(false);
 
      useEffect(() => {
 
@@ -32,21 +36,20 @@ const CardNoticias = ({img}) => {
   }, []);
 
   return (
-    <div className={styles.section}>
-      {items.map((item) => {
-        const { _id, img, body, date } = item;
-        
-
-
-   
-    return (
-        <div style={noticiasStyle}>
-        <CardImageNot img={img}/>
-        <CardBodyNot/>
-
-            
-        </div>
-    )
+    <div>
+        { 
+            /*items.map((item) => {
+                const { _id, img, body, date } = item;
+                return (
+                    <div style={noticiasStyle}>
+                        <CardImageNot img={img}/>
+                        <CardBodyNot/>                 
+                    </div>
+                )       
+            })*/
+        } 
+    </div>
+  )
 }
 
 export default CardNoticias
